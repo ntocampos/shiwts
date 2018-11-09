02789bea078cc179fa95105a15ac906b68d6ffdc
Moisés Neto <moises@guava.com.br>
2018-11-09 09:54:08 -0300

    Implements basic searchbox with alert on submit

--- src/components/Searchbox/index.js
+++ src/components/Searchbox/index.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, TextInput, Alert, Button } from 'react-native'
 
 import styles from './styles'
 
 class Searchbox extends React.Component {
+  state = { value: '' }
+
+  handleSubmit = (value) => Alert.alert(`Search button clicked w/ value "${value}"`)
+
   render() {
+    const { value } = this.state
+
     return (
-      <View>
-        <Text>@ Searchbox component</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter a title"
+          value={value}
+          autoCorrect={false}
+          onChangeText={(text) => this.setState({ value: text })}
+          onSubmitEditing={() => this.handleSubmit(value)}
+        />
+        <Button
+          title="Go!"
+          onPress={() => this.handleSubmit(value)}
+        />
       </View>
     )
   }
