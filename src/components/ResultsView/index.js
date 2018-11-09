dc92afd3985156b0300132a4ad138c7eb0e6602e
Moisés Neto <moises@guava.com.br>
2018-11-09 10:00:40 -0300

    Implements basic results list component

--- src/components/ResultsView/index.js
+++ src/components/ResultsView/index.js
@@ -2,18 +2,135 @@ import React from 'react'
 import {
   View,
   Text,
+  Image,
+  FlatList,
+  Alert,
+  TouchableHighlight,
 } from 'react-native'
 
 import styles from './styles'
+import Cancel from '../../../assets/cancel.png'
 
 class ResultsView extends React.Component {
-  render() {
+  keyExtractor = (item) => item.imdbID
+
+  handleSelect = (imdbID) => Alert.alert(`Title "${imdbID}" selected.`)
+
+  renderListItem = ({ item }) => {
+    const {
+      Title,
+      Year,
+      imdbID,
+      Type,
+      Poster,
+    } = item
+
     return (
-      <View>
-        <Text>@ ResultsView component</Text>
+      <TouchableHighlight
+        underlayColor="lightgray"
+        onPress={() => this.handleSelect(imdbID)}
+      >
+        <View style={styles.item}>
+          <Image
+            style={styles.poster}
+            defaultSource={Cancel}
+            source={{ uri: Poster }}
+          />
+          <View style={styles.description}>
+            <Text style={styles.title}>{Title}</Text>
+            <Text style={styles.subtitle}>({Year})</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+
+  render() {
+    const data = MOCK
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={data}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderListItem}
+        />
       </View>
     )
   }
 }
+
+const MOCK = [
+  {
+      Title: 'Batman Begins',
+      Year: '2005',
+      imdbID: 'tt0372784',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BZmUwNGU2ZmItMmRiNC00MjhlLTg5YWUtODMyNzkxODYzMmZlXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman v Superman: Dawn of Justice',
+      Year: '2016',
+      imdbID: 'tt2975590',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman',
+      Year: '1989',
+      imdbID: 'tt0096895',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman Returns',
+      Year: '1992',
+      imdbID: 'tt0103776',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BOGZmYzVkMmItM2NiOS00MDI3LWI4ZWQtMTg0YWZkODRkMmViXkEyXkFqcGdeQXVyODY0NzcxNw@@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman Forever',
+      Year: '1995',
+      imdbID: 'tt0112462',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNWY3M2I0YzItNzA1ZS00MzE3LThlYTEtMTg2YjNiOTYzODQ1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman & Robin',
+      Year: '1997',
+      imdbID: 'tt0118688',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMGQ5YTM1NmMtYmIxYy00N2VmLWJhZTYtN2EwYTY3MWFhOTczXkEyXkFqcGdeQXVyNTA2NTI0MTY@._V1_SX300.jpg'
+  },
+  {
+      Title: 'The Lego Batman Movie',
+      Year: '2017',
+      imdbID: 'tt4116284',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMTcyNTEyOTY0M15BMl5BanBnXkFtZTgwOTAyNzU3MDI@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman: The Animated Series',
+      Year: '1992–1995',
+      imdbID: 'tt0103359',
+      Type: 'series',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BNzI5OWU0MjYtMmMwZi00YTRiLTljMDAtODQ0ZGYxMDljN2E0XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman: Under the Red Hood',
+      Year: '2010',
+      imdbID: 'tt1569923',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BYTdlODI0YTYtNjk5ZS00YzZjLTllZjktYmYzNWM4NmI5MmMxXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg'
+  },
+  {
+      Title: 'Batman: The Dark Knight Returns, Part 1',
+      Year: '2012',
+      imdbID: 'tt2313197',
+      Type: 'movie',
+      Poster: 'https://m.media-amazon.com/images/M/MV5BMzIxMDkxNDM2M15BMl5BanBnXkFtZTcwMDA5ODY1OQ@@._V1_SX300.jpg'
+  }
+]
 
 export default ResultsView
